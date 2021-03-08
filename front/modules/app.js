f4ca662ea6aa2f3b3c767246900dac4ea9f9fc16
portfolio-y0711 <portfolio-y0711@gmail.com>
2021-03-08 11:19:23 +0900

    50 min past: inject module

--- front/modules/app.js
+++ front/modules/app.js
@@ -1,6 +1,15 @@
 class App {
+    loader
     constructor() { 
         console.log('[APP] Init')
+    }
+    async start() {
+        console.log('[APP] Start')
+        const instance = this.loader()
+    }
+    inject(loader) {
+        console.log(`[APP] ${loader} is Injectected`)
+        this.loader = loader
     }
 }
 
--- front/modules/finder.js
+++ front/modules/finder.js
@@ -1,28 +1,51 @@
+const finderLoader = () => new Finder()
+
+class Finder {
+    constructor() {
+        // constructor
+        console.log('[MOD] (Finder) Init')
+        this.init()
+    }
+    async init() {
+        console.log('init')
+        const wrapper = document.querySelector('finder')
+        const Api = window.API
+        // fetchData
+        const folder = (await Api.get('root'))[0]
+        const file = (await Api.get('1'))[0]
+        // console.log(folder)
+        // fetchData
+
+
+        // updateUI
+        const template = (data) => {
+            const { id, type } = data
+            const [filetype, icon] = 
+                (type === 'DIRECTORY') 
+                ? ['folder', 'folder']
+                : (type === 'FILE')
+                    ? ['file', 'file_present']
+                    : ['', '']
+            return (`
+                <div id="${id}" class="${filetype}">
+                    <i class="material-icons">${icon}
+                        <p class="cooltip">0 folders / 0 files</p>
+                    </i>
+                    <h1>Projects</h1>
+                </div> 
+            `)
+        }
+        wrapper.insertAdjacentHTML('beforeend',template(folder))
+        wrapper.insertAdjacentHTML('beforeend',template(file))
+        // updateUI
+
+        // cleanUI
+        // cleanUI
+    }
+}
 
 (async()=> {
     const App = window.APP 
     const Api = window.API
-    console.log('[MOD] (Finder) Init')
-    const wrapper = document.querySelector('finder')
-    const folder = (await Api.get('root'))[0]
-    const file = (await Api.get('1'))[0]
-    const template = (type) => {
-        const [filetype, icon] = 
-            (type === 'DIRECTORY') 
-            ? ['folder', 'folder']
-            : (type === 'FILE')
-                ? ['file', 'file_present']
-                : ['', '']
-        return (`
-            <div class="${filetype}">
-                <i class="material-icons">${icon}
-                    <p class="cooltip">0 folders / 0 files</p>
-                </i>
-                <h1>Projects</h1>
-            </div> 
-        `)
-    }
-    wrapper.insertAdjacentHTML('beforeend',template(folder.type))
-    wrapper.insertAdjacentHTML('beforeend',template(file.type))
-    
+    App.inject(finderLoader)
 })();
